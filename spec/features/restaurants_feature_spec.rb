dc417b646880e10dc79bfb22b8225f1acf9cc806
Sam Joseph <tansaku@gmail.com>
2014-07-03 10:40:01 +0100

    devise!

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -21,29 +21,46 @@ describe 'restaurants listing page' do
 end
 
 describe 'restaurant creation form' do
-  context 'input is valid' do
-    it 'should be able to create a restaurant' do
-      visit '/restaurants/new'
 
-      fill_in 'Name', with: 'Burger King'
-      fill_in 'Cuisine', with: 'Fast Food'
-      click_button 'Create Restaurant'
+  context 'logged out' do
+    it 'should forward user to sign in page' do
+      visit '/restaurants'
+      click_link 'Create restaurant'
 
-      expect(current_path).to eq '/restaurants'
-      expect(page).to have_content 'Burger King (Fast Food)'
+      expect(page).to have_content 'Sign in'
     end
+
   end
-  context 'input is not valid' do
-    it 'should be able to create a restaurant' do
-      visit '/restaurants/new'
 
-      fill_in 'Name', with: 'burger king'
-      fill_in 'Cuisine', with: 'ff'
-      click_button 'Create Restaurant'
+  context 'logged in' do
+    before do
+      user = User.create email: 's@s.com', password: '12345678', password_confirmation: '12345678'
+      login_as user
+    end
+    context 'input is valid' do
+      it 'should be able to create a restaurant' do
+        visit '/restaurants/new'
 
-      expect(current_path).to eq '/restaurants'
-      expect(page).not_to have_content 'burger king (ff)'
-      expect(page).to have_content 'has to start with a capital letter'
+        fill_in 'Name', with: 'Burger King'
+        fill_in 'Cuisine', with: 'Fast Food'
+        click_button 'Create Restaurant'
+
+        expect(current_path).to eq '/restaurants'
+        expect(page).to have_content 'Burger King (Fast Food)'
+      end
+    end
+    context 'input is not valid' do
+      it 'should be able to create a restaurant' do
+        visit '/restaurants/new'
+
+        fill_in 'Name', with: 'burger king'
+        fill_in 'Cuisine', with: 'ff'
+        click_button 'Create Restaurant'
+
+        expect(current_path).to eq '/restaurants'
+        expect(page).not_to have_content 'burger king (ff)'
+        expect(page).to have_content 'has to start with a capital letter'
+      end
     end
   end
 
@@ -52,22 +69,41 @@ end
 describe 'restaurant editing and deleting' do
   before {Restaurant.create name: 'KFC', cuisine: 'Chicken'}
 
-  it 'should be able to edit a restaurant' do
-    visit '/restaurants'
-    click_link 'Edit KFC'
+  context 'logged out' do
+    it 'should not be able to edit a restaurant' do
+      visit '/restaurants'
+      expect(page).not_to have_link 'Edit KFC'
+    end
 
-    fill_in 'Name', with: 'Kentucky Fried Chicken'
+    it 'should not be able to delete a restaurant' do
+      visit '/restaurants'
+      expect(page).not_to have_link 'Delete KFC'
+    end
 
-    click_button 'Update Restaurant'
-
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content 'Kentucky Fried Chicken'
   end
+  
+  context 'logged in' do
+    before do
+      user = User.create email: 's@s.com', password: '12345678', password_confirmation: '12345678'
+      login_as user
+    end
+    it 'should be able to edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
 
-  it 'can delete restaurants' do
-    visit '/restaurants'
-    click_link 'Delete KFC'
-    expect(page).not_to have_content 'KFC (Chicken)'
-    expect(page).to have_content 'Successfully deleted KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+
+      click_button 'Update Restaurant'
+
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+    end
+  
+    it 'can delete restaurants' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC (Chicken)'
+      expect(page).to have_content 'Successfully deleted KFC'
+    end
   end
 end    
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -9,22 +9,42 @@ end
 
 describe 'writing reviews' do
   before { Restaurant.create name: 'KFC', cuisine: 'Chicken' }
-  
-  it 'should add the review to the restaurant' do
-    leave_review('Not great', '2')
 
-    expect(page).to have_content 'Not great (★★☆☆☆)'
+  context 'logged out' do
+    it 'should not display the review form' do
+      visit '/restaurants'
+      expect(page).not_to have_field 'Thoughts'
+    end
+  end
+  
+  context 'logged in' do
+    before do
+      user = User.create email: 's@s.com', password: '12345678', password_confirmation: '12345678'
+      login_as user
+    end
+    it 'should add the review to the restaurant' do
+      leave_review('Not great', '2')
+
+      expect(page).to have_content 'Not great (★★☆☆☆)'
+    end
   end
 end
 
 describe 'average ratings' do
   before { Restaurant.create name: 'KFC', cuisine: 'Chicken' }
 
-  it 'calculates and displays the avg rating' do
-    leave_review('Poor', '2')
-    leave_review('Great', '4')
+  context 'logged in' do
+    before do
+      user = User.create email: 's@s.com', password: '12345678', password_confirmation: '12345678'
+      login_as user
+    end
 
-    expect(page).to have_content 'Average rating: ★★★☆☆'
+    it 'calculates and displays the avg rating' do
+      leave_review('Poor', '2')
+      leave_review('Great', '4')
+
+      expect(page).to have_content 'Average rating: ★★★☆☆'
+    end
   end
 
 end
